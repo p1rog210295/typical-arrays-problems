0814ebbde7f762275752d4c9e86378dd094d7f32
Evgeniy Deineka <nescafe210295@gmail.com>
2021-03-12 20:18:37 +0300

    feat:  add implementation of max , min , avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) return 0;
+  return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) return 0;
+  return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) return 0;
+  let sum = 0;
+  for (i = 0; i < array.length; i++) {
+    sum = array[i] + sum;
+  }
+  return sum / array.length;
 }
